1ae4b065a694d4d33fdaeb99d8da7064719bebdb
KelsCree <kelseycreehan@gmail.com>
2020-09-15 18:05:39 -0600

    practice filter build.

--- dogs_api/app/controllers/dogs_controller.rb
+++ dogs_api/app/controllers/dogs_controller.rb
@@ -1,8 +1,12 @@
 class DogsController < ApplicationController
 
   def index
-    @dogs = Dog.all
-    render json: @dogs
+    if params[:search]
+      @dogs = Dog.where('name LIKE ?', "%#{params[:search]}%")
+    else
+      @dogs = Dog.all
+    end
+      render json: @dogs
   end
 
 end
--- dogs_app/index.js
+++ dogs_app/index.js
@@ -1,13 +1,35 @@
+const searchParams = new URLSearchParams(window.location.search)
+const search = searchParams.get('search');
 console.log("JavaScript loaded");
 
-baseURL = "http://localhost:3000";
-dogsURL = `${baseURL}/dogs`;
+const baseURL = "http://localhost:3000"
+let dogsURL = `${baseURL}/dogs`
+
+
+if (search) {
+  dogsURL = `${dogsURL}?search=${search}`
+}
+
+
+
 
 const dogsSection = document.querySelector("section");
 
 fetch(dogsURL)
   .then(parseJSON)
   .then(displayDogs);
+
+  function displayDogs(dogs) {
+    if (dogs.length > 0){
+      dogs.forEach(showDog)
+    }
+    else {
+      const noDogs = document.createElement('p')
+      noDogs.textContent = 'no dogs match your criteria!
+      document.body.append(noDogs
+    }
+    }
+  }
 
 function displayDogs(dogs) {
   dogs.forEach(showDog);
